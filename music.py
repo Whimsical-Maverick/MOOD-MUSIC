7ca7ff44fdc73abbc73836823c02f4f117a6fa24
Bhavya <149354919+Whimsical-Maverick@users.noreply.github.com>
2024-12-06 14:16:45 +0530

    Delete music.py

--- music.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import webbrowser
-lang =st.text_input("Language")
-mood =st.text_input("Mood")
-btn= st.button("Recommend me song")
-if btn:
-    webbrowser.open(f"https://www.google.com/search?q={mood}+{lang}+songs+spotify&rlz=1C1VDKB_enIN1030IN1030&oq=happy+english+songs&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgkIARBFGDkYgAQyBwgCEAAYgAQyBggDEEUYQDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDIGCAcQRRg80gEJMTUwMzZqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8")
